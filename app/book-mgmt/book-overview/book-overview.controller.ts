908dd8103cefd5975d8712ada0df9b16531b24b9
Maciej Domaradzki <maciej.domaradzki@capgemini.com>
2016-09-02 10:27:00 +0200

    Empty Application

--- app/book-mgmt/book-overview/book-overview.controller.ts
+++
@@ -1,9 +0,0 @@
-export default class BookOverviewCtrl {
-  /* @ngInject */
-  constructor(private currentBooks:any) {
-  }
-
-  thereAreBooksToDisplay = function () {
-    return this.currentBooks && this.currentBooks.length > 0;
-  };
-}
--- app/general/general.module.ts
+++
@@ -1,6 +0,0 @@
-import appNavDirective from './navigation/app-nav.directive';
-
-let generalModule:ng.IModule = angular.module('general', [])
-  .directive('appNav', appNavDirective);
-
-export default generalModule.name;
