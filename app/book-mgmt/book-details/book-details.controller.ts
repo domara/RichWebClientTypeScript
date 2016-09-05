edff32c067de329d714443948b98def0f9fd289b
Maciej Domaradzki <maciej.domaradzki@capgemini.com>
2016-09-05 12:20:13 +0200

    Validation added

--- app/book-mgmt/book-details/book-details.controller.ts
+++ app/book-mgmt/book-details/book-details.controller.ts
@@ -1,9 +1,44 @@
 import {IBook} from '../../general/interfaces/book.interface';
 
 export class BookDetailsCtrl {
+  bookForm:any;
   book:IBook;
+
+  private static createErrorMessage(errorObject:any):string {
+  var errorCode;
+  if (errorObject) {
+    for (errorCode in errorObject) {
+      if (errorObject.hasOwnProperty(errorCode)) {
+        switch (errorCode) {
+          case 'required':
+            return 'Please provide a value';
+          case 'maxlength':
+            return 'The value is too long';
+          default:
+            return 'The value is wrong';
+        }
+      }
+    }
+  }
+};
   /* @ngInject */
   constructor(private currentBook:IBook) {
     this.book = this.currentBook;
   }
+
+  save():void {
+    let formIsValid = this.bookForm && this.bookForm.$valid;
+    alert(formIsValid);
+  }
+  getErrorMessageOfField(field:any):string {
+    let
+      errors = field && field.$error,
+      fieldIsInvalid = field && field.$invalid,
+      formIsSubmitted = this.bookForm && this.bookForm.$submitted;
+
+    if (fieldIsInvalid && (field.$touched || formIsSubmitted)) {
+      return BookDetailsCtrl.createErrorMessage(errors);
+    }
+  }
+
 }
